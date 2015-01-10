90ef47dd12a0bea1e63ce513ac29a092d8ef0879
Orta Therox <orta.therox@gmail.com>
2015-01-10 09:40:06 -0800

    support updating, and support only using the cocoadocs IP address

--- app.rb
+++ app.rb
@@ -22,46 +22,41 @@ class App < Sinatra::Base
   post '/pod/:name' do
     STDOUT.sync = true
     
-    # validate IP address against CP server?
+    # validate IP address against CP server if in production
+    if ENV['RACK_ENV'] != "development" && request.ip != "199.229.252.196"
+      halt 401, "You're not CocoaDocs!\n"
+    end
 
     pod = pods.where(pods[:name] => params[:name]).first    
-    if pod
-      
-      data = {
-        :pod_id => pod.id,
-        :total_files => params["total_files"],
-        :total_comments => params["total_comments"],
-        :total_lines_of_code => params["total_lines_of_code"],
-        :doc_percent => params["doc_percent"],
-        :readme_complexity => params["readme_complexity"],
-        :rendered_readme_url => params["rendered_readme_url"],
-        :initial_commit_date => params["initial_commit_date"],
-        :rendered_readme_url => params["rendered_readme_url"],
-        :not_found => 0,
-        :updated_at => Time.new,
-        :download_size => params["download_size"],
-        :license_short => params["license_short"],
-        :license_link => params["license_link"],
-      }
-      
-      metric = cocoadocs_pod_metrics.where(cocoadocs_pod_metrics[:pod_id] => pod.id).first
-      if metric
-        puts "found"
-        
-        # TODO: Updates
-        
-        # data[:id] = metric.id
-        # cocoadocs_pod_metrics.update(data).kick.to_json
-        metric.update(data).kick.to_json
-      else
-        puts "not found"
-        data[:created_at] = Time.new
-        cocoadocs_pod_metrics.insert(data).kick.to_json
-      end
-
-    else
-      halt 401
+    unless pod
+      halt 404, "Pod not found for #{params[:name].}"      
     end
+      
+    data = {
+      :pod_id => pod.id,
+      :total_files => params["total_files"],
+      :total_comments => params["total_comments"],
+      :total_lines_of_code => params["total_lines_of_code"],
+      :doc_percent => params["doc_percent"],
+      :readme_complexity => params["readme_complexity"],
+      :rendered_readme_url => params["rendered_readme_url"],
+      :initial_commit_date => params["initial_commit_date"],
+      :rendered_readme_url => params["rendered_readme_url"],
+      :not_found => 0,
+      :updated_at => Time.new,
+      :download_size => params["download_size"],
+      :license_short_name => params["license_short_name"],
+      :license_canonical_url => params["license_canonical_url"],
+    }
+    
+    # update or create a metrics
+    metric = cocoadocs_pod_metrics.where(cocoadocs_pod_metrics[:pod_id] => pod.id).first
+    if metric
+      cocoadocs_pod_metrics.update(data).where(id: metric.id).kick.to_json
+    else
+      data[:created_at] = Time.new
+      cocoadocs_pod_metrics.insert(data).kick.to_json
+    end
+    
   end
-
 end
